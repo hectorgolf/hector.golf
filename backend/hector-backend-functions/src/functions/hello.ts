8952865aff60edb711be49021ffff94d439c3712
Lasse Koskela <lasse.koskela@gmail.com>
2024-06-12 12:19:30 +0300

    Tweak the hi function

--- backend/hector-backend-functions/src/functions/hello.ts
+++ backend/hector-backend-functions/src/functions/hello.ts
@@ -1,59 +1,25 @@
-import { Headers } from 'undici';
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { getAccessToken } from '../utils/request-processing';
-import { generateAccessToken } from '../utils/authentication';
 
-async function parseHeaders(headers: Headers, context: InvocationContext): Promise<{[key:string]:string[]}> {
-    const headersObject: {[key:string]:string[]} = {};
-    headers.forEach((value, key) => {
-        if (!Array.isArray(headersObject[key])) {
-            headersObject[key] = [];
-        }
-        headersObject[key].push(value);
-        context.log(`Header => ${JSON.stringify(key)}: ${JSON.stringify(value)}`)
-    });
-    return Promise.resolve(headersObject);
-}
 
 export async function sayHello(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}" (${request.method})`);
-
-    const reqHeaders = await parseHeaders(request.headers, context);
-    const bodyString = await request.text()
-    const bodyJson = JSON.parse(bodyString)
-
-    context.log(`Request headers: ${JSON.stringify(reqHeaders)}`)
-    context.log(`Request body as String: ${JSON.stringify(bodyString)}`)
-    context.log(`Request body as JSON:   ${JSON.stringify(bodyJson)}`)
-
-    const name = bodyJson.thisis || request.query.get('name') || bodyString || 'world';
-
-    let expiryDate = new Date();
-    expiryDate.setDate(expiryDate.getDate() + 30);
-
-    var accessToken = generateAccessToken(name);
-    let accessCookieConfig = `accesstoken=${accessToken}; Expires=${expiryDate.toUTCString()};`
-    accessCookieConfig += ' HttpOnly;'
-    accessCookieConfig += ' Secure;'
-
     const token = await getAccessToken(request, context)
     if (!token) {
         return {
-            body: `You're not logged in, ${name}! Here's a token inside a cookie for you: ${accessToken}`,
-            headers: {
-                'Set-Cookie': accessCookieConfig
-            }
+            status: 403,
+            body: `You're not logged in. You should go to ${request.url.replace(/\/api\/.*$/, '/api/authorize')}`,
         };
     } else {
         return {
-            body: `Hello, ${name}! You're already logged in - no cookie for you!`
+            status: 200,
+            body: `Hello! I see that you've already logged in.`,
         };
     }
 };
 
 app.http('sayHello', {
     methods: ['GET', 'POST'],
-    route: 'hi/{name:alpha?}',
+    route: 'hi',
     authLevel: 'anonymous',
     handler: sayHello
 });
