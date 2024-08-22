454d5c9581ccc5deb8bb1081adae78e72c7678d0
Lasse Koskela <lasse.koskela@gmail.com>
2024-08-22 15:01:30 +0300

    Try to figure out why importing another .ts file does not work

--- astrosite/scripts/nightly.sh
+++ astrosite/scripts/nightly.sh
@@ -2,6 +2,7 @@
 
 echo "This is the nightly script running on $(date)"
 echo "I have an environment variable TEETIME_USERNAME=$TEETIME_USERNAME"
-
-echo "Now trying to run 'src/scripts/nightly.ts' with 'ts-node'..."
+echo "The directory tree looks like this:"
+tree -P '*.ts' -P '*.js' -P '*.sh' -P '*.json' -I '__azurite*' --gitignore --prune
+echo "Now trying to run 'src/scripts/nightly.ts' with 'tsx'..."
 npx tsx src/scripts/nightly.ts
