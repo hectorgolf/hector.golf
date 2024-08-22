eb69afabffc777ee5ca7f10c7ae7e332ed0ce26e
Lasse Koskela <lasse.koskela@gmail.com>
2024-08-22 14:44:19 +0300

    Update nightly.sh: Invoke tsx with npx

--- astrosite/scripts/nightly.sh
+++ astrosite/scripts/nightly.sh
@@ -4,4 +4,4 @@ echo "This is the nightly script running on $(date)"
 echo "I have an environment variable TEETIME_USERNAME=$TEETIME_USERNAME"
 
 echo "Now trying to run 'src/scripts/nightly.ts' with 'ts-node'..."
-tsx src/scripts/nightly.ts
+npx tsx src/scripts/nightly.ts
