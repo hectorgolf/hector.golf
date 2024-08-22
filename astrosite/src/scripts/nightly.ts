912b274b9826eea116dfab9f737d118b11c7c965
Lasse Koskela <lasse.koskela@gmail.com>
2024-08-22 15:11:41 +0300

    Remove the "astro:content" import from nightly.ts

--- astrosite/src/scripts/nightly.ts
+++ astrosite/src/scripts/nightly.ts
@@ -3,11 +3,25 @@ import { join } from 'path';
 
 import { getPlayerHandicap } from '../code/handicaps.ts';
 import { getPlayerById } from '../code/players.ts';
-import { type Player } from '../schemas/players.ts';
 
 const pathToPlayersJson = join(__dirname, '../data/players.json');
 
+type Player = {
+    id: string,
+    name: {
+        first: string,
+        last: string
+    },
+    contact: {
+        phone: string
+    },
+    image?: string,
+    club?: string,
+    handicap?: number,
+}
+
 const playersJson = JSON.parse(readFileSync(pathToPlayersJson, 'utf-8').toString());
+console.log(`Attempting to update handicap data for ${playersJson.length} players...`)
 const playerListPromises = playersJson.map((player: any) => {
     const playerObject = getPlayerById(player.id)
     if (playerObject) {
@@ -24,5 +38,6 @@ const playerListPromises = playersJson.map((player: any) => {
 })
 
 Promise.all(playerListPromises).then((players) => {
+    console.log(`Updated ${players.length} players JSON:`)
     console.log(JSON.stringify(players, null, 2))
 })
