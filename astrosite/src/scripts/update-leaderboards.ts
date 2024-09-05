a7c5c07c9e97e72659642a8b4ad15443be5f2f5b
Lasse Koskela <lasse.koskela@gmail.com>
2024-09-06 00:05:28 +0300

    Get events for "update-leaderboards.ts" without Astro-related imports

--- astrosite/src/scripts/update-leaderboards.ts
+++ astrosite/src/scripts/update-leaderboards.ts
@@ -1,17 +1,34 @@
-import { getAllEventsGroupedByChronology, parseEventDateRange } from '../code/events.ts'
+import { parseEventDateRange, isoDate} from '../code/dates.ts'
 import { fetchHectorLeaderboardData, fetchVictorLeaderboardData } from '../code/leaderboards/google-sheets.ts'
-import type { HectorEvent } from '../schemas/events.ts'
 import { updateHectorEventLeaderboard } from '../code/leaderboards/github.ts'
 
+import eventsData from '../data/events.json'
+
+
+type HectorEvent = {
+    id: string,
+    name: string,
+    format: string,
+    date: string,
+    leaderboardSheetId: string|undefined
+}
 
 const updateLeaderboardsForAllOngoingTournaments = async () => {
-    const { ongoing, past } = getAllEventsGroupedByChronology(e => e.format === 'hector' && !!(e as HectorEvent).leaderboardSheetId)
-    const recent = past.filter(e => {
-        const { endDate } = parseEventDateRange(e.date) || {}
-        return !!endDate && endDate < new Date() && endDate > new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 2)
-    })
-    const relevantEvents = ongoing.concat(recent)
-    for (let event of relevantEvents) {
+    const events = (eventsData as Array<HectorEvent>)
+        .filter(e => e.format === 'hector')
+        .map(e => e as HectorEvent)
+        .filter(e => !!(e.leaderboardSheetId))
+        .filter(e => {
+            const { startDate, endDate } = parseEventDateRange(e.date) || {}
+            if (!startDate) return false
+            if (!endDate) return false
+            if (isoDate(startDate) > isoDate(new Date())) return false  // event hasn't even started yet
+            if (isoDate(endDate) < isoDate(new Date(new Date().getTime() - 1000 * 60 * 60 * 24))) return false  // event finished yesterday or earlier
+            return true
+        })
+
+    console.log(`Found ${events.length} ongoing Hector events with a live leaderboard Google Sheet: ${events.map(e => e.name).join(', ')}`)
+    for (let event of events) {
         console.log(`Updating leaderboard for ${event.name}...`)
         const hectorEvent = event as HectorEvent
         const leaderboardSheetId = hectorEvent.leaderboardSheetId
