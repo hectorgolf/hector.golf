2e92c35f014330f3e52713cc8c1ee79a9542fd17
Lasse Koskela <lasse.koskela@gmail.com>
2024-09-05 22:13:01 +0300

    Remove scripts/nightly.sh as an unnecessary middleman between Github Action and TypeScript

--- astrosite/scripts/nightly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "This is the nightly script running on $(date)"
-echo "I have an environment variable TEETIME_USERNAME=$TEETIME_USERNAME"
-echo "The directory tree looks like this:"
-tree -P '*.ts' -P '*.js' -P '*.sh' -P '*.json' -I '__azurite*' --gitignore --prune
-echo "Now trying to run 'src/scripts/nightly.ts' with 'tsx'..."
-npx tsx src/scripts/nightly.ts
