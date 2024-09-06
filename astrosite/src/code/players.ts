a9a22e43705732a138ab7e4074a88d9fa4a3ac4d
Lasse Koskela <lasse.koskela@gmail.com>
2024-09-07 00:02:04 +0300

    Shorten everybody's last names

--- astrosite/src/code/players.ts
+++ astrosite/src/code/players.ts
@@ -44,9 +44,86 @@ export function getPlayerHandicapById(id: string): number|undefined {
     return events[events.length - 1].handicap;
 }
 
+// const longestCommonPrefix = (strings: Array<string>): number => {
+//     const longest = Math.max(...strings.map(s => s.length))
+//     for (let length = 1; length <= longest; length++) {
+//         const uniques = [...new Set<string>(strings.map(s => s.slice(0, length)))]
+//         if (uniques.length === strings.length) {
+//             return length - 1
+//         }
+//     }
+//     return 0
+// }
+
+const shortestUniquePrefix = (strings: Array<string>): number => {
+    const longest = Math.max(...strings.map(s => s.length))
+    for (let length = 1; length <= longest; length++) {
+        const uniques = [...new Set<string>(strings.map(s => s.slice(0, length)))]
+        if (uniques.length === strings.length) {
+            return length
+        }
+    }
+    console.warn(`There is no unique prefix for ${JSON.stringify(strings)}`)
+    return 0
+}
+
+/**
+ * Let's compute a set of mappings for player names that are privacy-sensitive, i.e. their
+ * last names should be shortened. We will use the shortest unique prefix of the last names
+ * to figure out how much of the last name to keep. If there are no duplicate first names
+ * among players with shortened last names, we will keep the full first name and take just
+ * the first letter of their last name. If there are e.g. a "John Deere" and a "John Dillinger",
+ * we'd need to keep two letters of their last name to distinguish the two individuals.
+ */
+type PlayerName = { first: string, last: string }
+type NameShorteningFunction = (playerOrName: Player|PlayerName|string) => string
+const shortenLastName: NameShorteningFunction = (() => {
+    const playersByFirstName = new Map<string, Array<Player>>();
+    const shortenedLastNamesById = new Map<string, string>();
+    const shortenedLastNamesByFullName = new Map<string, string>();
+    getAllPlayers().forEach(player => {
+        const firstName = player.name.first;
+        if (!playersByFirstName.has(firstName)) {
+            playersByFirstName.set(firstName, []);
+        }
+        playersByFirstName.get(firstName)?.push(player);
+    })
+    for (let firstName of playersByFirstName.keys()) {
+        const playersWithSameFirstName = playersByFirstName.get(firstName) || []
+        const playersWithShortenedLastName = playersWithSameFirstName.filter(player => player.privacy === 'shorten-last-name')
+        const lastNames = playersWithShortenedLastName.map(player => player.name.last.toUpperCase())
+        const prefixLength = shortestUniquePrefix(lastNames)
+        playersWithShortenedLastName.forEach(player => {
+            shortenedLastNamesById.set(player.id, player.name.last.slice(0, prefixLength))
+            shortenedLastNamesByFullName.set(`${player.name.first} ${player.name.last}`, player.name.last.slice(0, prefixLength))
+        })
+    }
+    return (playerOrName: Player|PlayerName|string): string => {
+        if (typeof(playerOrName) === 'string') {
+            return shortenedLastNamesById.get(playerOrName) || shortenedLastNamesByFullName.get(playerOrName) || '?'
+        }
+        if (typeof(playerOrName) === 'object') {
+            const obj = playerOrName as any
+            if (obj.id && obj.name) {
+                const { id, name } = obj
+                return shortenedLastNamesById.get(id) || name.last.slice(0, 1)
+            }
+            if (obj.first && obj.last) {
+                const { first, last } = obj
+                const mapping = shortenedLastNamesByFullName.get(`${first} ${last}`)
+                if (mapping) {
+                    return mapping
+                }
+                console.warn(`No mapping for ${JSON.stringify(playerOrName)} among ${JSON.stringify(Array.from(shortenedLastNamesByFullName.keys()))} or  ${JSON.stringify(Array.from(shortenedLastNamesById.keys()))}`)
+            }
+        }
+        throw new Error(`Unsupported parameter to shortenLastName: ${typeof(playerOrName)} ${JSON.stringify(playerOrName)}`)
+    }
+})()
+
 const renderPlayerName = (name: { first: string, last: string }, privacySetting?: string): string => {
     if (privacySetting === 'shorten-last-name') {
-        return `${name.first} ${name.last.slice(0, 1)}`
+        return `${name.first} ${shortenLastName(name)}`
     }
     return `${name.first} ${name.last}`
 }
