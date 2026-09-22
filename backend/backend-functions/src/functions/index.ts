cab3811bad4dcf49413115d9b20d6dc0b9925ac2
Lasse Koskela <lasse.koskela@gmail.com>
2026-09-22 23:49:47 +0300

    Screenshot for the hole descriptions PR

--- backend/backend-functions/src/functions/index.ts
+++
@@ -1,6 +0,0 @@
-import ExtractScorecardInformation from "./scorecard-detection";
-import GeneratePlayerBiography from "./generate-player-biography";
-import GeneratePlayerAvatar from "./generate-player-avatar";
-import TournamentLeaderboard from "./tournament-leaderboard";
-
-export { ExtractScorecardInformation, GeneratePlayerBiography, GeneratePlayerAvatar, TournamentLeaderboard };
